61de9d1f06dec0326c0cf511a7580b4eb8524518
CyberKim0 <iloanyajoshua5@gmail.com>
2026-09-07 12:11:43 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -38,7 +38,10 @@ const rest = new REST({ version: "10" }).setToken(
     console.log("🔄 Registering slash commands...");
 
     await rest.put(
-      Routes.applicationCommands(process.env.CLIENT_ID),
+      Routes.applicationGuildCommands(
+  process.env.CLIENT_ID,
+  process.env.GUILD_ID
+)
       { body: commands }
     );
 
